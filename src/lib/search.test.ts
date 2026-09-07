1027d7bc2ad0fcabd57ac4aaae2fe73895e5f513
getto-dev <sailordag@gmail.com>
2026-09-08 02:57:42 +0300

    Fix search normalization test

--- src/lib/search.test.ts
+++ src/lib/search.test.ts
@@ -3,7 +3,7 @@ import { searchCatalog, tokenizeQuery } from './search';
 import { CATALOG } from './catalog';
 
 describe('search', () => {
-  test('normalizes Russian query words', () => expect(tokenizeQuery('монтаж радиатора')).toEqual(['монтаж','радиат']));
+  test('normalizes Russian query words', () => expect(tokenizeQuery('монтаж радиатора')).toEqual(['монтаж', 'радиатор']));
   test('ranks name matches above unrelated descriptions', () => {
     const result = searchCatalog(CATALOG, 'радиатор');
     expect(result[0]?.name).toContain('радиатор');
