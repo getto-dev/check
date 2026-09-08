d2c48e6ce3739874e8c286885593002a4d4cdfcd
getto-dev <sailordag@gmail.com>
2026-09-08 21:18:26 +0300

    Copy Unicode PDF font into static build

--- scripts/generate-version.mjs
+++ scripts/generate-version.mjs
@@ -1,9 +1,15 @@
-import { readFile, writeFile } from 'node:fs/promises';
+import { mkdir, readFile, writeFile, copyFile } from 'node:fs/promises';
 
 const pkg = JSON.parse(await readFile('package.json', 'utf8'));
 const template = await readFile('public/sw.js.template', 'utf8');
 
 await writeFile('public/version.json', JSON.stringify({ version: pkg.version }, null, 2) + '\n');
 await writeFile('public/sw.js', template.replaceAll('__VERSION__', pkg.version));
+
+await mkdir('public/fonts', { recursive: true });
+await copyFile(
+  'node_modules/@fontsource/roboto/files/roboto-all-400-normal.woff',
+  'public/fonts/roboto-all-400-normal.woff',
+);
 
 console.log(`Generated PWA assets for version ${pkg.version}`);
