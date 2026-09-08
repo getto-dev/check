5114407c9b7dc12f1ef04516e3f27943fbdd2d7a
getto-dev <sailordag@gmail.com>
2026-09-08 22:48:19 +0300

    add shared estimate constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,31 @@
-import packageJson from '../../package.json';
+export const APP_VERSION = '24.1.0';
+export const MAX_DISCOUNT_PERCENT = 50;
 
-export const APP_VERSION = packageJson.version;
+export const ESTIMATE_LAYOUT = {
+  pageWidth: 595.28,
+  pageHeight: 841.89,
+  marginX: 34,
+  top: 34,
+  nameRight: 380,
+  quantityLeft: 380,
+  quantityRight: 435,
+  priceLeft: 435,
+  priceRight: 500,
+  totalLeft: 500,
+  summaryLeft: 360,
+  text: {
+    title: 10,
+    object: 8,
+    section: 8,
+    name: 9,
+    description: 7,
+    values: 8,
+    grandLabel: 10,
+    grandValue: 13,
+  },
+  row: {
+    minHeight: 30,
+    nameLineHeight: 11,
+    descriptionLineHeight: 9,
+  },
+} as const;
